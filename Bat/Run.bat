9f87a55cb88276050bb900a55d8ee3749009e4f4
ManyakRus <30662875+ManyakRus@users.noreply.github.com>
2021-12-21 17:28:29 +0300

    1

---
+++ Bat/Run.bat
@@ -0,0 +1,1 @@
+start CMD /K docker run -p 5672:5672  --name RabbitMQ -v c:\Docker\RabbitMQ\Data:/var/lib/rabbitmq rabbitmq
---
+++ Bat/run_manager.bat
@@ -0,0 +1,1 @@
+docker run -d --hostname my-rabbit --name rabbit_manager -p 8080:15672 rabbitmq:3-management
